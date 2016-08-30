b2ec8fe1ac4c6ce2bb54f2314874cf7ae82007d1
Nguyen <vnguyen629@student.cccd.edu>
2016-08-30 16:35:11 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/vnguyen629cs273/yarrworld/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.vnguyen629cs273.yarrworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+    }
+}
